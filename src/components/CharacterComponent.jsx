442a51b733bd1f9c56be7b09078c1e646f2839b5
Dfyerman <153417503+Dfyerman@users.noreply.github.com>
2024-08-01 22:12:25 -0700

    added files to make project work

--- src/components/CharacterComponent.jsx
+++ src/components/CharacterComponent.jsx
@@ -1,1 +1,32 @@
-j
+// src/components/CharacterComponent.jsx
+import React, { useState } from 'react';
+import './character.css'; // Adjust the path if necessary
+
+function CharacterComponent() {
+  const [direction, setDirection] = useState('face-up');
+
+  const toggleDirection = () => {
+    setDirection((prevDirection) => (prevDirection === 'right' ? 'left' : 'right'));
+  };
+
+  return (
+    <div>
+      <div className="Character">
+        <img
+          className="Character_shadow pixelart"
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacterShadow.png"
+          alt="Shadow"
+        />
+        <img
+          id="character"
+          className={`Character_spritesheet pixelart face-${direction}`}
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/21542/DemoRpgCharacter.png"
+          alt="Character"
+        />
+      </div>
+      <button onClick={toggleDirection}>Toggle Direction</button>
+    </div>
+  );
+}
+
+export default CharacterComponent;
