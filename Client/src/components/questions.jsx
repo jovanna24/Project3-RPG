c0e9baa9d5ffc78a3707356a5d0199249870b1bc
ahurst123 <austinhurst123@gmail.com>
2024-08-06 18:42:06 -0700

    Added some story elements, all the way to the end.

--- Client/src/components/questions.jsx
+++ Client/src/components/questions.jsx
@@ -1,15 +1,27 @@
 export const questions = [
     {
-      question: "How Would You Like To Start A Fire",
-      optionA: "Use Matches",
-      optionB: "Find A Rock",
+      question: "You wake up from your nap on your flight to Denver, you are going to visit your parents after not seeing them for months since they moved from Phoenix to retire in the mountains. You take a look around and realize that you have a rising feeling in your belly similar to how you feel when you crest a hill on a roller coaster. The rising feeling gets stronger and stronger.  You are feeling nervous now. That rising sensation from earlier now turns into weightlessness. At this point the captain announces himself over the intercom and says, “Prepare yourselves for a rough landing! BRACE BRACE BRACE!” You are sitting in the emergency exit row, looking out your window and you can see you are roughly 150 feet from the ground.  Do you pull the emergency exit and jump right, or do you brace with everyone else and hope the Colorado Rockies are forgiving to a plane crash landing?",
+      optionA: "Jump out of the Emergency Exit?",
+      optionB: "Brace for Landing?",
       correctAnswer: "A"
     },
     {
-      question: "What is 2 + 2?",
-      optionA: "4",
-      optionB: "3",
+      question: "You jumped out of the plane at just the right time and landed in a grove of Douglas Fir trees, slowing your fall until you hit the ground. You take a second to breathe, taking in the gravity of the situation at hand. Your watch says it is 3pm.  Seeing as it is summer you only have about 4 to 5 hours of daylight left. As you just evacuated out of the plane without thinking about anyone else's safety, you do not have anything on you besides what's in your pockets. Some gum, a paper clip, your wallet, and your phone. Your phone is cracked and broken, it won’t power on. The first things to focus on must be food, water, and then shelter. After surveying the land, you see you have two options: Enter a cave and hopefully find water, or venture higher to get a better look at where you are. Cave on the left or mountain on the right?",
+      optionA: "Cave?",
+      optionB: "Mountain?",
       correctAnswer: "A"
+    },
+    {
+      question: "You have entered the cave. The mount of the cave is easily 50 feet in diameter.  As the light dims as you descend deeper, you can hear what sounds like flowing water! However the cave splits into two separate paths.  The entrance to the right path is only a small hole. Barely big enough to crawl through.  The sunlight barely shines through.  20 feet down the hole you can see rushing water, and what looks like a larger cavern you can stand up and turn around in. The left path looks like a vertical slab of rock reminding you of El Capitan in Yosemite, that you would have to scale to get up, at least 3 times the height of yourself.. And while looking easier, you are unsure if the following pathway even leads to the water you see. Climbing on the left or Crawling on the right?",
+      optionA: "Climb?",
+      optionB: "Crawl?",
+      correctAnswer: "B"
+    },
+    {
+      question: "You get down on your knees and examine the space.  You start crawling through and as you make your way, the passage narrows as you get closer to the sound of the rushing water, your vision dims as the sunlight is blocked out by your body. You keep trusting that you will make it, remembering that it won’t be much farther. The sound of rushing water fills your ears, as you get closer and closer. After a grueling 10 minute long crawl, you make it to the end, and the passage is opening up. With the little light you have, you examine the room around you on your hands and knees as the passage is not big enough to stand in. All you can see is rushing, moving water about 10 feet below you. You are parched after jumping from the plane and the brutal crawl through the cave, but there is one problem: there is nowhere to stand where you can reach it.  You have a choice, to turn left and go back, or to take a leap into the water on your right.",
+      optionA: "Turn around?",
+      optionB: "Leap into the river?",
+      correctAnswer: "B"
     },
     // Add more questions here...
   ];
