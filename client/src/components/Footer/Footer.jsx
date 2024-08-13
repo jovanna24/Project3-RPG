ebddc793a4ee2b5ae13d5e14ffe2d4f11bff413b
jovanna24 <77941035+jovanna24@users.noreply.github.com>
2024-08-12 22:16:12 -0700

    added home routing on footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,44 +1,55 @@
-import './Footer.css';
-import Logo from '../../assets/Logo.svg';
-import Ig_Icon from '../../assets/ig_icon.svg';
-import X_icon from '../../assets/x_icon.svg';
-import LinkedIn_icon from '../../assets/linkedin_icon.svg';
+import "./Footer.css";
+import Logo from "../../assets/Logo.svg";
+import Ig_Icon from "../../assets/ig_icon.svg";
+import X_icon from "../../assets/x_icon.svg";
+import LinkedIn_icon from "../../assets/linkedin_icon.svg";
+import { Link, useNavigate } from "react-router-dom";
 
 const Footer = () => {
+
+    const [menu, setMenu] = useState("home");
+
+
   return (
     <div className="footer" id="footer">
-        <div className="footer-content">
-            <div className="footer-conent-left">
-                <img src={Logo} alt="logo" className='footer-logo' />
-                <div className="footer-social-icons">
-                    <img src={Ig_Icon} alt="instagram" />
-                    <img src={X_icon} alt="x" />
-                    <img src={LinkedIn_icon} alt="linkedin" />
-                </div>
-            </div>
-            <div className="footer-content-center">
-                <h2>COMPANY</h2>
-                <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Privacy Policy</li>
-                </ul>
-            </div>
-            <div className="footer-content-right">
-                <h2>
-                    GET IN TOUCH
-                </h2>
-                <ul>
-                    <li>+1-212-456-7890</li>
-                    <li>contact@RPG.com</li>
-                </ul>
-            </div>
+      <div className="footer-content">
+        <div className="footer-conent-left">
+          <img src={Logo} alt="logo" className="footer-logo" />
+          <div className="footer-social-icons">
+            <img src={Ig_Icon} alt="instagram" />
+            <img src={X_icon} alt="x" />
+            <img src={LinkedIn_icon} alt="linkedin" />
+          </div>
         </div>
-        <div className="copyright">
-            <p className="footer-copyright">Copyright 2024 © RPG.com - All Rights Reserved.</p>
-            </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <Link
+              to="/"
+              onClick={() => setMenu("home")}
+              className={menu === "home" ? "active" : ""}
+            >
+              <li>Home</li>
+            </Link>
+            <li>About Us</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+1-212-456-7890</li>
+            <li>contact@RPG.com</li>
+          </ul>
+        </div>
+      </div>
+      <div className="copyright">
+        <p className="footer-copyright">
+          Copyright 2024 © RPG.com - All Rights Reserved.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Footer;
